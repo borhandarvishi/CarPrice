234cdf5eb22a1098f1eb318ddad317cc5565b3e7
Borhan Darvishi <borhandr23@gmail.com>
2023-10-31 18:26:36 +0330

    Rename the variable in app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import json
 import pickle
-
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-regmodel=pickle.load(open('RFRegressorModel.pkl','rb'))
+RFRegressorModel =pickle.load(open('RFRegressorModel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
@@ -19,7 +18,7 @@ def predict_api():
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    output= RFRegressorModel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
@@ -28,8 +27,8 @@ def predict():
     data=[float(x) for x in request.form.values()]
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    output=RFRegressorModel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The Car price prediction is {}".format(output))
 
 
 
